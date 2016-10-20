f6000a4c50f45dfca63fa02e663e90ac88055d0b
Thomas GERVAIS <thomas.gervais@st.com>
2016-10-20 12:56:45 +0200

    Structure Reorganisation

---
+++ common/GlobalAssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("PerspicApps")]
+[assembly: AssemblyProduct("PA.Utilities")]
+[assembly: AssemblyCopyright("(c) PA 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
