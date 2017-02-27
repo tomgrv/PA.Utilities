6c499037fa4966cd72d184313c73d414c89b3136
Thomas GERVAIS <thomas.gervais@st.com>
2017-02-27 15:13:05 +0100

    Rewrite script parser

--- PA.Utilities.InnoSetupTask/InnoSetup/ScriptParser.cs
+++ PA.Utilities.InnoSetupTask/InnoSetup/ScriptParser.cs
@@ -21,53 +21,8 @@ namespace PA.Utilities.InnoSetupTask.InnoSetup
             }
         }
 
-        class InnoScriptDataParser : IniDataParser
-        {
-            private string currentSection = string.Empty;
-
-            protected override string ExtractKey(string s)
-            {
-                if (this.currentSection != "setup")
-                {
-                    return s;
-                }
-
-                return base.ExtractKey(s);
-            }
-
-            protected override string ExtractValue(string s)
-            {
-                if (this.currentSection != "setup")
-                {
-                    return string.Empty;
-                }
-
-                return base.ExtractValue(s);
-            }
-
-            protected override bool LineMatchesAKeyValuePair(string line)
-            {
-                if (this.currentSection != "setup" && Regex.IsMatch(line, "^([a-zA-Z]+\\s*:|#[a-z]+\\s+)"))
-                {
-                    return true;
-                }
-
-                return base.LineMatchesAKeyValuePair(line);
-            }
-
-
-
-
-
-            protected override void ProcessSection(string line, IniData currentIniData)
-            {
-                base.ProcessSection(line, currentIniData);
-                this.currentSection = line.Trim().Trim('[', ']').ToLower();
-            }
-        }
-
         public ScriptParser()
-            : base(new InnoScriptDataParser())
+            : base(new DataParser())
         {
             this.Parser.Configuration.KeyValueAssigmentChar = '=';
             this.Parser.Configuration.AssigmentSpacer = "";
