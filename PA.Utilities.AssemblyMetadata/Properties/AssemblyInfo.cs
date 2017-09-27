078b8b390039d12bbcb8c811910b83417a32407b
Thomas GERVAIS <thomas gervais@st.com>
2017-09-27 23:12:37 +0200

    Add AssemblyMetadata project

---
+++ PA.Utilities.AssemblyMetadata/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using PA.Utilities.AssemblyMetadata;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("PA.Utilities.AssemblyMetadata")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyAuthors("thomas.gervais@gmail.com")]
+
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
