d711d5caf7d702f3cc464d7ee60026874aa9a9db
Thomas GERVAIS <thomas.gervais@st.com>
2017-10-02 23:10:38 +0200

    Add Common.Logging support Allow package generation

--- PA.Utilities/Properties/AssemblyInfo.cs
+++ PA.Utilities/Properties/AssemblyInfo.cs
@@ -1,3 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using PA.Utilities.AssemblyMetadata;
+
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("PA.Utilities")]
+[assembly: AssemblyDescription("Generic extensions for object parsing and convertion")]
+[assembly: AssemblyAuthors("thomas.gervais@gmail.com")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
