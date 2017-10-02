0dca7dd06ca2f463b541d57b4113fe9c5c8ccc53
Thomas GERVAIS <thomas.gervais@st.com>
2017-10-02 23:11:10 +0200

    Allow Package generation

--- PA.Utilities.AssemblyMetadata/Properties/AssemblyInfo.cs
+++ PA.Utilities.AssemblyMetadata/Properties/AssemblyInfo.cs
@@ -1,13 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using PA.Utilities.AssemblyMetadata;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("PA.Utilities.AssemblyMetadata")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyAuthors("thomas.gervais@gmail.com")]
 
 
 // The following attributes are used to specify the signing key for the assembly, 
