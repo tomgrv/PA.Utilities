73bc1555a4a591fe4a8e341b4e1b793df769d592
Thomas GERVAIS <thomas.gervais@gmail.com>
2020-10-21 12:51:16 +0200

    chore(deps): update packages and migrate to package reference

--- PA.Utilities.AssemblyMetadata/Properties/AssemblyInfo.cs
+++ PA.Utilities.AssemblyMetadata/Properties/AssemblyInfo.cs
@@ -6,8 +6,9 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("PA.Utilities.AssemblyMetadata")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generic extensions for assembly metadata handling and package generation")]
 [assembly: AssemblyAuthors("tomgrv")]
+
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
